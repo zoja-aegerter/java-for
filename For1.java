de5827b9823c02ea5436b585c441855005bb53f8
Zoja Aegerter <97830485+zoja-aegerter@users.noreply.github.com>
2022-10-13 18:18:54 +0200

    Add for loop

---
+++ For1.java
@@ -0,0 +1,14 @@
+
+public class For1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        for (int j = 1; j <= 10; j++) {
+            if (j == 5) {
+                System.out.println("Hey du hast mitch gefunden " + j);
+            }
+        }
+    }
+}
